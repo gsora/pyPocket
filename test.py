da4cf2b329dad93e442f16cedd878ed67de76c0d
Gianguido Sorà <gianguidorama@gmail.com>
2014-02-12 20:43:48 +0100

    added json parsing and printing

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 
 import pyPocket
 import os
+import json
+import time
 
 consumerKey = "YOUR-CONSUMER-KEY"
 redirectUri = "http://gsora.me"
@@ -24,7 +26,24 @@ if(not os.path.isfile("myLoginData")):
 file = open("myLoginData", "r+")
 aTok = file.readline()
     
-# now let's see some data
+# now let's parse some data
 gData = pyPocket.getData(consumerKey, aTok)
-print(gData)
+
+# load the json
+pocketJson = json.loads(gData)
+
+# print article id, title, url, date of add, word count, total entry
+
+entryCounter = 0
+
+for i in pocketJson["list"]:
+    print("Pocket Item ID: {}".format(pocketJson["list"][i]["resolved_id"]))
+    print("Article title: {}".format(pocketJson["list"][i]["resolved_title"]))
+    print("Article URL: {}".format(pocketJson["list"][i]["resolved_url"]))
+    print("Added the: {}".format(time.ctime(int(pocketJson["list"][i]["time_added"]))))
+    print("Word count: {}\n\n".format(pocketJson["list"][i]["word_count"]))
+    entryCounter+=1
+
+print("JSON: {} item/s".format(entryCounter))
+
 file.close()
